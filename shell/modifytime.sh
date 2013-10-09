0033af814908a5ba96b956ecce2048f81a52f439
Will Foster <wfoster@redhat.com>
2013-10-09 11:25:53 -0400

    cleaner to simply exist if the watched file does not exist, you would not be able to know it's unique md5sum anyway.

--- shell/modifytime.sh
+++ shell/modifytime.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # note changes in a file
 # you need to know the md5sum of the watchfile
-# change file or log variables to your liking
+# change watchfile or logfile variables to suit your needs
 
 watchfile=/tmp/test.txt
 logfile=/tmp/test.log
-filemd5='d8e8fca2dc0f896fd7cb4cb0031ba249'
+filemd5='76f0aa47381d7ee6e20ec3a9b11aecab'
 date=$(/bin/date +%Y%m%d-%M%S)
 writelog="/usr/bin/echo '${watchfile} has changed on ${date}' >> ${logfile}"
 unmodified=$(md5sum $watchfile | grep $filemd5 | wc -l)
 
-# create watched file if it doesn't exist
-if [ ! -e $watchfile ] ; then
-   echo "your monitored file does not exist"
+# fail and alert if the watchfile does not exist
+{
+if [ ! -f $watchfile ]; then
+    echo "your watched file does not exist, create it and note md5sum"
+    exit 0
 fi
+}
 
 # create logfile if it doesn't exist
 if [ ! -e $logfile ] ; then
