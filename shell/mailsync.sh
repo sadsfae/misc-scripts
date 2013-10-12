d04e1c9d47b8da19a9833e7776377b19d229e1d8
Will Foster <wfoster@redhat.com>
2013-10-12 17:17:51 -0400

    mail should be synced if you can do a DNS lookup against your remote mailserver, not if you have a VPN interface up. this will work with a wired connection, say in a local office since you may not be using VPN

--- shell/mailsync.sh
+++ shell/mailsync.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-# sync offlineimap if you're connected to a VPN
-# also check if it's running (or hung) and kill it off first
-imapactive=`ps -ef | grep offlineimap | grep -v grep | wc -l`
-vpnactive=`/sbin/ifconfig -a | grep tun0 | wc -l`
-mailsync="/usr/bin/offlineimap -u quiet -q"
+# sync offineimap when you can do a DNS lookup against your mailserver
+# run this from cron, I use every 12min (*/12 * * * *)
 
-# kill offlineimap if active, sometimes it hangs
+imapactive=`ps -ef | grep offlineimap | grep -v grep | wc -l`
+mailsync="/usr/bin/offlineimap -u quiet -q"
+username=`whoami`
+mailhost=`cat /home/$username/.offlineimaprc | grep remotehost | awk '{print $3}'`
+online=`host $mailhost | grep "has address" | wc -l`
+
+# kill offlineimap if active, in some rare cases it may hang
 case $imapactive in
 '1')
    killall offlineimap && sleep 5
 ;;
 esac
 
-# if there's a TUN interface then sync mail bro
-case $vpnactive in
+# if you can do a DNS lookup, sync your mail
+case $online in
 '1')
    $mailsync
 ;;
