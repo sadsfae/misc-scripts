6523a5175e22a9619386a2cd05ae254352ac6b97
Will Foster <wfoster@redhat.com>
2014-06-13 12:06:17 +0200

    also calculate average transfer rate

--- shell/calculate-vsftpd-transfer.sh
+++ shell/calculate-vsftpd-transfer.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # calculate amount transferred in KB and GB for a given vsftpd user
+# calculate the average transfer rate across all transfers (after normalized by vsftpd)
 # usage ./calculate-vsftp-transfer.sh $username
 # assumes the following log format options for vsftpd
 # xferlog_enable=YES
@@ -21,16 +22,27 @@ calculate_gb_transfer()
     echo "(($kb_transfer) / (1024*1024))" | bc
 }
 
+calculate_transfer_average()
+{   # gather total mount of transfers
+    transfer_count=`cat $log_location | grep $username | grep "OK DOWNLOAD" | grep bytes \
+    | awk -F "," '{print $NF}' | sed 's/Kbyte\/sec//' | wc -l`
+    transfer_speed_sum=`cat $log_location | grep $username | grep "OK DOWNLOAD" | grep bytes \
+    | awk -F "," '{print $NF}' | sed 's/Kbyte\/sec//' | awk '{s+=$1} END {print s}'`
+    echo "$transfer_speed_sum / $transfer_count" | bc
+}    
+
 calculate_transfer_total()
 {   # generate summary
     kb_transfer=$(calculate_kb_transfer)
     gb_transfer=$(calculate_gb_transfer)
+    avg_transfer=$(calculate_transfer_average)
     echo "                                      "
     echo "======================================"	
     echo "Total Download for user $username"
     echo "--------------------------------------"
-    echo "Kilobytes :: $kb_transfer"
-    echo "Gigabytes :: $gb_transfer"
+    echo "Kilobytes    :: $kb_transfer"
+    echo "Gigabytes    :: $gb_transfer"
+    echo "Average Rate :: $avg_transfer KB/sec" 
     echo "======================================"
 }
 
