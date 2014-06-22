123ccadd8c29b732b4076ad0d6d3ff57a8341d89
Will Foster <wfoster@redhat.com>
2014-06-22 13:39:25 +0200

    super simple 3minute list, index manipulation example

---
+++ python/jbond.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# simple insert and indexing for list of james bond
+
+import sys
+
+bondlist = ['craig', 'connery', 'moore']
+print 'James Bond actors in order of coolness are:', bondlist[:]
+ 
+choice = raw_input("Add your bond: ")
+ 
+def bond(bondlist):
+    bondlist.insert(0, choice)
+    print "New list of bonds is", bondlist
+    bondlist.reverse()
+    print ".. in order of least cool to coolest is", bondlist
+    return bondlist
+
+def main():
+    return bond(bondlist)
+
+if __name__ == '__main__':
+    main()
