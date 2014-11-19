2d23bc848e486442239212986a41a37008841109
Will Foster <wfoster@redhat.com>
2014-11-18 19:14:59 -0500

    openstack data, instance and database backup script

---
+++ shell/openstack-svc-backup.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+# script to backup config files, user data, instances etc prior
+# to upgrading OpenStack to a new release.
+# backup nova, glance, cinder, neutron, keystone configs, database
+# requires $myssh key forwarding and keys in place
+# requires NFS storage mounted on the host under /mnt/backups/$myhost
+
+oslab_nodes=(host07 host08 host09 host10 host11 host12 host13 host14) 
+oslab_controller=(host02)
+oslab_networker=(host03)
+oslab_storage=(host04 host05 host06)
+dump_date=$(/bin/date +%Y%m%d%H%M)
+mysqldump=`which mysqldump`
+mysqldump_opts='--all-databases --opt --single-transaction --master-data'
+myssh='ssh -o StrictHostKeyChecking=false'
+
+#### BEGIN BACKUPS ####
+
+## NOVA ##
+oslab_backup_etc_nova() {
+   $myssh root@$x "rsync -av --progress /etc/nova /mnt/backups/$x/" 2>&1 >/dev/null 
+}
+
+for x in ${oslab_nodes[@]}; do
+echo "backing up /etc/nova on $x";
+oslab_backup_etc_nova; 
+echo "/etc/nova backup complete on $x";
+done
+
+oslab_backup_var_lib_nova() {
+   $myssh root@$x "rsync -av --progress /var/lib/nova /mnt/backups/$x/" 2>&1 >/dev/null
+}
+
+for x in ${oslab_nodes[@]}; do
+echo "backing up /var/lib/nova on $x";
+oslab_backup_var_lib_nova;
+echo "/var/lib/nova backup complete on $x";
+done
+
+## GLANCE ##
+oslab_backup_var_lib_glance() {
+   $myssh root@$x "rsync -av --progress /srv/gluster/glance /mnt/backups/$x/" 2>&1 >/dev/null
+}
+
+for x in ${oslab_storage[@]}; do
+echo "backing up /srv/gluster/glance on $x";
+oslab_backup_var_lib_glance; 
+echo "/srv/gluster/glance backup complete on $x";
+done
+
+## CINDER ##
+oslab_backup_var_lib_cinder() {
+   $myssh root@$x "rsync -av --progress /srv/gluster/cinder /mnt/backups/$x/" 2>&1 >/dev/null
+}
+
+for x in ${oslab_storage[@]}; do
+echo "backing up /srv/gluster/cinder on $x";
+oslab_backup_var_lib_cinder; 
+echo "/srv/gluster/cinder backup complete on $x";
+done
+
+## NEUTRON ##
+oslab_backup_etc_neutron() {
+   $myssh root@$x "rsync -av --progress /etc/neutron /mnt/backups/$x/" 2>&1 >/dev/null
+}
+
+for x in ${oslab_networker[@]}; do
+echo "backing up /etc/neutron on $x";
+oslab_backup_etc_neutron;
+echo "/etc/neutron backup complete on $x";
+done
+
+## KEYSTONE ##
+oslab_backup_etc_keystone() {
+   $myssh root@$x "rsync -av --progress /etc/keystone /mnt/backups/$x/" 2>&1 >/dev/null
+}
+
+for x in ${oslab_controller[@]}; do
+echo "backing up /etc/keystone on $x";
+oslab_backup_etc_keystone; 
+echo "/etc/keystone backup complete on $x";
+done
+
+## DATABASE ##
+oslab_backup_mysql() {
+   $myssh root@$x "$mysqldump $mysqldump_opts 2>&1 >/dev/null | /usr/bin/gzip - > \
+   /mnt/backups/$x/mysqldump-oslab-${dump_date}.gz"
+}
+
+#### END BACKUPS ####
+#### BEGIN DB BACKUPS ####
+
+for x in ${oslab_controller[@]}; do
+echo "backing up MySQL database on $x";
+oslab_backup_mysql; 
+echo "MySQL backup complete on $x";
+done
+
+for x in ${oslab_networker[@]}; do
+echo "backing up MySQL database on $x";
+oslab_backup_mysql; 
+echo "MySQL backup complete on $x";
+done
+
+#### END DB BACKUPS ####
