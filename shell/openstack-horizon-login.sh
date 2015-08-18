2e83b59ac8a0876064b98c0ed0bd2f88cbd0eb4b
Will Foster <wfoster@redhat.com>
2015-08-18 12:15:11 +0100

    sanitize input, add further comments

--- shell/openstack-horizon-login.sh
+++ shell/openstack-horizon-login.sh
@@ -7,27 +7,36 @@ horizonhost=$1
 username=$2
 password=$3
 
-url=http://$horizonhost
+# print usage if not specified
+if [[ $# -eq 0 ]]; then
+        echo "USAGE: ./openstack-horizon-login.sh \$HORIZONHOST \$USERNAME \$PASSWORD"
+        echo "       ./openstack-horizon-login.sh 10.0.0.1 admin yourpassword"
+        echo "                                                              "
+        exit 1
+fi
 
+url=http://$horizonhost
 tmpfile=`mktemp /tmp/horizonXXXXXX`
 cookies=`mktemp /tmp/cookiesXXXXXX`
 cookies2=`mktemp /tmp/cookies2XXXXXX`
 results=`mktemp /tmp/resultsXXXXXX`
 postfile=`mktemp /tmp/postfileXXXXXX`
 
+# obtain working copy of site
 wget --save-headers --keep-session-cookies --save-cookies=$cookies -q -O - $url > $tmpfile
 
+# munge out OpenStack related fields
 region=`grep 'name="region" value=' $tmpfile  | awk -F\" '{ print $6 }' | sed -e 's/:/%3A/g' -e 's,/,%2F,g'`
-
 token=`grep csrfmiddlewaretoken $tmpfile | sed "s/.*value='\(.*\)'.*/\1/g"`
 
+# login and record results
 echo "csrfmiddlewaretoken=$token&region=$region&username=$username&password=$password" > $postfile
-
 wget --save-headers -q -O - --load-cookies=$cookies --keep-session-cookies --save-cookies=$cookies2 --post-data="csrfmiddlewaretoken=$token&region=$region&username=$username&password=$password" $url/dashboard/auth/login/ > $results
 
+# cleanup
 rm -f $tmpfile $cookies $cookies2 $postfile
 
-# assume "/dashboard/project/" should be in the results.  This indicates a login success
+# assume "/dashboard/project/" should be in the results, this indicates sucess  
 if grep -q "/dashboard/project/" $results ; then
   echo OK Login succeeded
   exitstatus=0
@@ -36,5 +45,6 @@ else
   exitstatus=2
 fi
 
+# final cleanup and exit
 rm -f $results
 exit $exitstatus
