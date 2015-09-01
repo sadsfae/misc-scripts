71426c05fe55cdc06c79cbf796e0e7aba50b184c
Will Foster <wfoster@redhat.com>
2015-09-01 16:36:54 +0100

    track ipaddress via router or floatingip-list

--- shell/openstack-track-fip.sh
+++ shell/openstack-track-fip.sh
@@ -6,7 +6,7 @@ fip_address=$1
 # print usage if variable isn't specified
 if [[ $# -eq 0 ]]; then
         echo "USAGE: ./openstack-track-fip.sh \$IPADDRESS"
-        echo "       ex: ./openstack-track-fip.sh 8.8.8.8"
+        echo "       ./openstack-track-fip.sh 128.136.179.9"
         echo "                                          "
         exit 1
 fi
@@ -16,16 +16,25 @@ source /root/keystonerc_admin
 
 track_ip() {
         targrouter=$(neutron router-list | grep -w $fip_address | awk '{print $2}')
-
+        targfipid=$(neutron floatingip-list | grep -w $fip_address | awk '{print $2}')
+        
 # if there's no fip, no need to look further
-if [[ -z $targrouter ]]; then
+if [ -z "$targrouter" -a -z "$targfipid" ]; then
 	echo "IP address not found, or unassociated"
 	echo "                                     "
 	exit 1
 fi
-        targtenant=$(neutron router-show $targrouter | grep tenant_id | awk '{print $4}')
+
+# floating ip will either show against router-show or floating-ip list
+if [ -z $targfipid ]; then
+	targtenant=$(neutron router-show $targrouter | grep tenant_id | awk '{print $4}')
         targname=$(openstack project show $targtenant | grep name | awk '{print $4}')
         targmail=$(openstack user show $targname | grep email | awk '{print $4}')
+else
+        targtenant=$(neutron floatingip-show $targfipid | grep tenant_id | awk '{print $4}')
+        targname=$(openstack project show $targtenant | grep name | awk '{print $4}')
+        targmail=$(openstack user show $targname | grep email | awk '{print $4}')
+fi
 
 cat << EndofMessage
 
@@ -41,7 +50,7 @@ cat << EndofMessage
 #                                               
 ################################################
 
-To disable the account run from controller:
+To disable the account run:
 ------------------------------------------------
 
 source /root/keystonerc_admin
