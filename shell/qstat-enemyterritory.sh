8f67a3b3e46ad9a76c893152269a60ee688796df
Will Foster <wfoster@redhat.com>
2015-09-30 13:37:49 +0100

    Also generate player info and frags

--- shell/qstat-enemyterritory.sh
+++ shell/qstat-enemyterritory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# generates simple CSS table with populated qstat data from a gameserver
-# requires qstat
+# generate a simple HTML table with quakestat (qstat) data
+# requires quakestat
 # requires convert (from imagemagick), html2ps for image conversion
 
 gameserver="example.com"
@@ -12,21 +12,26 @@ qstat_cleanup() {
 	if [ -f /tmp/et.html ]; then
 		rm -f /tmp/et.html
 	fi
-	if [ -f /tmp/et.ps ]; then
+    	if [ -f /tmp/et.ps ]; then
+		rm -f /tmp/et.ps
+	fi
+    	if [ -f /tmp/etfull.txt ]; then
 		rm -f /tmp/et.ps
 	fi
 }
 
 qstat_generate() {
-	/usr/bin/quakestat -woets $gameserver -raw "|" > /tmp/et.txt
-	srv=`cat /tmp/et.txt | awk -F "|" '{print $2}'`
-	pc=`cat /tmp/et.txt | awk -F "|" '{print $6}'`
-	pclimit=`cat /tmp/et.txt | awk -F "|" '{print $5}'`
-	gmap=`cat /tmp/et.txt | awk -F "|" '{print $4}'`
-	gping=`cat /tmp/et.txt | awk -F "|" '{print $7}'`
-	srvtype=`cat /tmp/et.txt | awk -F "|" '{print $3}'`
+	# generate generic status
+    /usr/bin/quakestat -woets $gameserver -P -raw "|" > /tmp/et.txt
+	srv=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $2}'`
+	pc=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $6}'`
+	pclimit=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $5}'`
+	gmap=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $4}'`
+	gping=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $7}'`
+	srvtype=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $3}'`
 
-	cat > /tmp/et.html <<EOF
+    # generate generic CSS template for status
+    cat > /tmp/et.html <<EOF
 <style type="text/css">
 .tg  {border-collapse:collapse;border-spacing:0;border-color:#ccc;}
 .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#fff;}
@@ -45,15 +50,22 @@ qstat_generate() {
   </tr>
   <tr>
     <td class="tg-24i8">$srv</td>
-    <td class="tg-24i8">$pc/$pclimit</td>
+    <td class="tg-24i8">$pc of $pclimit</td>
     <td class="tg-24i8">$gmap</td>
     <td class="tg-24i8">$gping</td>
     <td class="tg-24i8">$srvtype</td>
   </tr>
 </table>
 EOF
-	cp /tmp/et.html /home/`whoami`/public_html/et.html
-        /usr/bin/html2ps /tmp/et.html > /tmp/et.ps
+    
+    # generate player info and frags and append
+	/usr/bin/quakestat -woets funcamp.net -P | tail -n+3 > /tmp/etfull.txt 
+    cat /tmp/etfull.txt | awk 'BEGIN{print "<table>"} {print "<tr>";for(i=1;i<=NF;i++)print \
+		"<td>" $i"</td>";print "</tr>"} END{print "</table>"}' >> /tmp/et.html
+    # HTML copy  
+    cp /tmp/et.html /home/`whoami`/public_html/et.html
+    # create image of HTML page
+    /usr/bin/html2ps /tmp/et.html > /tmp/et.ps
 	convert /tmp/et.ps /home/`whoami`/public_html/et.png
 }
 
