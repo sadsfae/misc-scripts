daa65a880df6c836381234ea076dd244c3632995
Will Foster <wfoster@redhat.com>
2015-10-02 14:52:08 +0100

    revamp: added recent players scraped from logs, better CSS

--- shell/qstat-enemyterritory.sh
+++ shell/qstat-enemyterritory.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # generate a simple HTML table with quakestat (qstat) data
+# this will also now parse logs and print recent players over past 24hrs
 # requires quakestat for HTML, convert and html2ps for image conversion
 # I use the following .html2psrc
 #BODY {
@@ -7,9 +8,27 @@
 #     }
 
 gameserver="example.com"
-tmpfiles=(/tmp/et.txt /tmp/et.ps /tmp/et.html /tmp/etfull.txt)
 
-qstat_cleanup() {
+# variables for qstat generation
+ETHTML="/tmp/et.html"
+ETFULL="/tmp/etfull.txt"
+ETTXT="/tmp/et.txt"
+ETPS="/tmp/et.ps"
+ETHOMEHTML="/home/`whoami`/public_html/et.html"
+ETHOMEIMG="/home/`whoami`/public_html/et.png"
+
+# variables for recent players
+DATE=`date +%Y%m%d`
+ETLOG="/home/`whoami`/ET/et-server-$DATE.log"
+ETPLAYERLOG="/tmp/etplayers.txt"
+ETPLAYERSTRIP="/tmp/etplayerstrip.txt"
+ETPLAYERHTML="/tmp/etplayerstrip.html"
+ETPLAYERSHORT="/tmp/etplayerstripshort.txt"
+
+tmpfiles=($ETHTML $ETFULL $ETTXT $ETPS $ETPLAYERLOG $ETPLAYERSTRIP \
+          $ETPLAYERHTML $ETPLAYERSHORT)
+
+cleanup_files() {
 for i in ${tmpfiles[*]}; do
         if [ -e $i ]; then
                 rm -f $i
@@ -19,16 +38,16 @@ done
 
 qstat_generate() {
 	# generate generic status
-        /usr/bin/quakestat -woets $gameserver -P -raw "|" > /tmp/et.txt
-	srv=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $2}'`
-	pc=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $6}'`
-	pclimit=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $5}'`
-	gmap=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $4}'`
-	gping=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $7}'`
-	srvtype=`cat /tmp/et.txt | awk -F "|" 'NR==1{print $3}'`
+        /usr/bin/quakestat -woets $gameserver -P -raw "|" > $ETTXT
+	srv=`cat $ETTXT | awk -F "|" 'NR==1{print $2}'`
+	pc=`cat $ETTXT | awk -F "|" 'NR==1{print $6}'`
+	pclimit=`cat $ETTXT | awk -F "|" 'NR==1{print $5}'`
+	gmap=`cat $ETTXT | awk -F "|" 'NR==1{print $4}'`
+	gping=`cat $ETTXT | awk -F "|" 'NR==1{print $7}'`
+	srvtype=`cat $ETTXT | awk -F "|" 'NR==1{print $3}'`
 
         # generate generic CSS template for status
-        cat > /tmp/et.html <<EOF
+        cat > $ETHTML <<EOF
 <style type="text/css">
 .tg  {border-collapse:collapse;border-spacing:0;border-color:#ccc;}
 .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#fff;}
@@ -42,7 +61,7 @@ qstat_generate() {
     <th class="tg-edkc">PLAYERS</th>
     <th class="tg-edkc">MAP</th>
     <th class="tg-edkc">PING<br></th>
-    <th class="tg-edkc">MOD/TYPE</th>
+    <th class="tg-edkc">SERVER NAME</th>
   </tr>
   <tr>
     <td class="tg-24i8">$srv</td>
@@ -53,19 +72,70 @@ qstat_generate() {
   </tr>
 </table>
 EOF
+
+    # generate generic CSS for table title
+    cat >> $ETHTML << EOF
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;border-color:#ccc;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#fff;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;    background-color:#f0f0f0;}
+.tg .tg-edkc{font-size:28px;vertical-align:top}
+</style>
+<table class="tg">
+  <tr>
+    <th class="tg-edkc">Current Players<br></th>
+</tr>
+<br>
+EOF
     
     # generate player info and frags and append
-    /usr/bin/quakestat -woets $gameserver -P | tail -n+3 > /tmp/etfull.txt 
+    /usr/bin/quakestat -woets $gameserver -P | tail -n+3 > $ETFULL 
     # replace 'frags' with 'experience' since that's what qstat reports
     sed -i -e 's/frags/experience/' /tmp/etfull.txt
-    cat /tmp/etfull.txt | awk 'BEGIN{print "<table>"} {print "<tr>";for(i=1;i<=NF;i++)print \
-		"<td>" $i"</td>";print "</tr>"} END{print "</table>"}' >> /tmp/et.html
-    # HTML copy  
-    cp /tmp/et.html /home/`whoami`/public_html/et.html
-    # create image of HTML page
-    /usr/bin/html2ps -F /tmp/et.html > /tmp/et.ps
-    convert /tmp/et.ps /home/`whoami`/public_html/et.png
+    cat $ETFULL | awk 'BEGIN{print "<table>"} {print "<tr>";for(i=1;i<=NF;i++)print \
+		"<td>" $i"</td>";print "</tr>"} END{print "</table>"}' >> $ETHTML 
 }
 
-qstat_cleanup
+daily_players() {
+        # get list of daily players in form of "G_xpsave_add: saved xp for $player"
+        cat $ETLOG | grep "saved xp" | egrep -v "Smitty|Heinrich|Tyrone|Hoganmueller| \
+                Bloogada|Galgoci|Deiter|Nelson|Hasselhoff|Leopold" | sort | uniq -u > $ETPLAYERLOG
+        # strip out only the players names, this will also pick up spaces
+        cat $ETPLAYERLOG | awk '{$1=$2=$3=$4=""; print $0}' > $ETPLAYERSHORT
+}
+
+generate_playerhtml() {
+        # generate generic CSS for table title
+        cat > $ETPLAYERHTML << EOF
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;border-color:#ccc;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#fff;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;    background-color:#f0f0f0;}
+.tg .tg-edkc{font-size:28px;vertical-align:top}
+</style>
+<table class="tg">
+  <tr>
+    <th class="tg-edkc">Recent Players<br></th>
+</tr>
+<br>
+EOF
+        # generate nice HTML of recent players
+        cat $ETPLAYERSHORT | awk 'BEGIN{print "<table>"} {print "<tr>";for(i=1;i<=NF;i++)print \
+                "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' >> $ETPLAYERHTML
+}
+
+html_convert() {
+    # merge the two HTML files
+    cat $ETPLAYERHTML >> $ETHTML  
+    # HTML copy  
+    cp $ETHTML $ETHOMEHTML
+    # create image of HTML page
+    /usr/bin/html2ps -F $ETHTML > $ETPS
+    convert $ETPS $ETHOMEIMG
+}
+
+cleanup_files
 qstat_generate
+daily_players
+generate_playerhtml
+html_convert
