b28f69c4ef75d3aa72980badbc5660901c8e4a95
Will Foster <wfoster@redhat.com>
2017-06-21 19:26:56 +0100

    update mailsync script to check if imap can be contacted

--- shell/mailsync.sh
+++ shell/mailsync.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-# sync offineimap when you can do a DNS lookup against your mailserver
-# run this from cron, I use every 12min (*/12 * * * *)
+# sync offlineimap if you have connection to the internet
+# and you can ping your imap server successfully.
 
+imapserver=$(cat ~/.offlineimaprc | grep remotehost | awk '{print $3}')
 imapactive=`ps -ef | grep offlineimap | grep -v grep | wc -l`
+netactive=$(ping -c3 $imapserver >/dev/null 2>&1 && echo up || echo down)
 mailsync="/usr/bin/offlineimap -u quiet -q"
-username=`whoami`
-mailhost=`cat /home/$username/.offlineimaprc | grep remotehost | awk '{print $3}'`
-online=`host $mailhost | grep "has address" | wc -l`
 
-# kill offlineimap if active, in some rare cases it may hang
+# kill offlineimap if active, sometimes it hangs
 case $imapactive in
 '1')
-   killall offlineimap && sleep 5
+    killall offlineimap && sleep 5
 ;;
 esac
 
-# if you can do a DNS lookup, sync your mail
-case $online in
-'1')
-   $mailsync
+# Check that you can access the SMTP server
+case $netactive in
+'up')
+    $mailsync
+;;
+
+'down')
+    :
 ;;
 esac
